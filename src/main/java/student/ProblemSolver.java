b992dca382cdc91ef60ed631f71e299f9dfa8b48
Sondre Bolland <sondre.bolland@uib.no>
2024-10-11 15:37:52 +0200

    Remove git history

---
+++ src/main/java/student/ProblemSolver.java
@@ -0,0 +1,27 @@
+package student;
+
+import java.util.ArrayList;
+
+import graph.*;
+
+public class ProblemSolver implements IProblem {
+
+	@Override
+	public <V, E extends Comparable<E>> ArrayList<Edge<V>> mst(WeightedGraph<V, E> g) {
+		// Implement me :)
+		return null;
+	}
+
+	@Override
+	public <V> V lca(Graph<V> g, V root, V u, V v) {
+		// Implement me :)
+		return null;
+	}
+
+	@Override
+	public <V> Edge<V> addRedundant(Graph<V> g, V root) {
+		// Implement me :)
+		return null;
+	}
+
+}
